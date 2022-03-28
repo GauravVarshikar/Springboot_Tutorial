deef39ba58ca1e1cbd48ad39c57eb9642bd01b88
Gaurav Varshikar <gaurav.varshikar@gmail.com>
2022-03-28 14:56:05 +0530

    basic CRUD operations

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,18 +1,22 @@
 package com.example.demo.controller;
 
+import com.example.demo.jdbc.EmployeeDAO;
+import com.example.demo.jdbc.EmployeeDAOImpl;
 import com.example.demo.pojo.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Locale;
 
 @RestController
 public class EmployeeController {
 
-    @GetMapping("/message")
-    public String getMessage(){
-        return "Hello World!";
-    }
+    @Autowired
+    EmployeeDAO dao;
 
+    @GetMapping("/employees")
+    public String getEmployees(){
+        dao.findAllEmployees();
+        return "All employees retrieved";
+    }
     @GetMapping("/employees/{name}")
     public Employee getEmployees(@PathVariable String name) {
         return new Employee(1, "Gaurav", "Varshikar", 10000);
@@ -22,6 +26,7 @@ public class EmployeeController {
     public Employee createEmployee(@RequestBody Employee employee) {
         employee.setFirstName(employee.getFirstName().toUpperCase());
         employee.setLastName(employee.getLastName().toUpperCase());
+        dao.addEmployee(employee);
         return employee;
     }
 }
--- src/main/java/com/example/demo/jdbc/EmployeeDAOImpl.java
+++ src/main/java/com/example/demo/jdbc/EmployeeDAOImpl.java
@@ -3,8 +3,11 @@ package com.example.demo.jdbc;
 import com.example.demo.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -15,7 +18,9 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     @Override
     public int addEmployee(Employee employee) {
         String add = "insert into Employee1 values (?,?,?,?)";
-        int rows = template.update(add, employee.getId(), employee.getFirstName(), employee.getLastName(), employee.getSalary());
+        System.out.println(add);
+        //int rows = template.update(add, employee.getId(), employee.getFirstName(), employee.getLastName(), employee.getSalary());
+        int rows = 1;
         return rows;
     }
 
@@ -36,6 +41,15 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public List<Employee> findAllEmployees() {
+        System.out.println("select * from Employee1");
+//        template.query("select * from Employee1", (resultSet, i) -> {
+//            Employee e = new Employee();
+//            e.setId(resultSet.getInt(1));
+//            e.setFirstName(resultSet.getString(2));
+//            e.setLastName(resultSet.getString(3));
+//            e.setSalary(resultSet.getInt(4));
+//            return e;
+//        });
         return null;
     }
 }
